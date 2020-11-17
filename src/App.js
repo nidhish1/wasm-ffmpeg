d5a206e2dee342de9a49719a0dcfe48ba4e69903
nidhish1 <eyesgotshow@gmail.com>
2020-11-18 01:59:24 +0530

    added gif conversion

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   const [ready, setReady] =useState(false)
   const [video,setVideo] = useState() 
+  const [gif,setGif] = useState()
   const load = async() => {
     await ffmpeg.load()
     setReady(true)
@@ -17,15 +18,25 @@ function App() {
     load()
    },[]);
 
+  
+  const convertToGif = async () => {
+    ffmpeg.FS ('writeFile','test.mp4',await fetchFile(video))
+    await ffmpeg.run('-i','test.mp4','-t','10','-f','gif','out.gif')
+    const data = ffmpeg.FS('readFile', 'out.gif')
+
+    const url = URL.createObjectURL(new Blob([data.buffer]), {type : 'image/gif'})
+    setGif(url)
+  }
+
   return ready ?(
     <div className="App">
       {video &&  <video controls
                   width="250"
-                  src = {URL.createObjectURL(video)}
-      > 
-                  
-
+                  src = {URL.createObjectURL(video)}> 
                 </video>}
+
+       <button onClick = {convertToGif}>CONVERT</button> 
+       {gif && <img src = {gif} />}
       
       
       <input type = "file" onChange = {(e) => setVideo (e.target.files?.item(0))}/>
