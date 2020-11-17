c4aadea0a3b14fd3e1af4bcd232820bede98a50a
nidhish1 <eyesgotshow@gmail.com>
2020-11-18 01:31:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
+import {createFFmpeg, fetchFile} from '@ffmpeg/ffmpeg'
+const ffmpeg = createFFmpeg( {log:true})
+
 function App() {
-  return (
+
+  const [ready, setReady] =useState(false)
+  const [video,setVideo] = useState() 
+  const load = async() => {
+    await ffmpeg.load()
+    setReady(true)
+  }
+  useEffect(() => {   
+    load()
+   },[]);
+
+  return ready ?(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {video &&  <video controls
+                  width="250"
+                  src = {URL.createObjectURL(video)}
+      > 
+                  
+
+                </video>}
+      
+      
+      <input type = "file" onChange = {(e) => setVideo (e.target.files?.item(0))}/>
+
     </div>
-  );
+  ) :  (<p> Loading</p>)
 }
 
 export default App;
